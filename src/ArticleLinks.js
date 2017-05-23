ff8e68803a88e0483662ca336847780c3b39432b
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-23 10:51:06 -0400

    Show/hide comments

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -2,15 +2,51 @@ import React, { Component } from 'react';
 import './ArticleLinks.css';
 
 class ArticleLinks extends Component {
+  constructor() {
+    super();
+    this.state = {showComments: false, comments: []};
+  }
+
   addComment(e) {
     e.preventDefault();
-    console.log(this.commentText.value);
+    const comments = [...this.state.comments];
+    comments.push(this.commentText.value);
+    this.setState({ comments });
+  }
+
+  renderComments() {
+    if (this.state.showComments) {
+      return (
+        <div>
+          <form onSubmit={this.addComment.bind(this)}>
+            <textarea className="comment" placeholder="Type your comment here" ref={(input) => this.commentText = input}></textarea>
+            <button type="submit" className="article-link">
+              <i className="fa fa-check"></i>
+              <span className="article-link-text">Submit</span>
+            </button>
+          </form>
+          <ul className="comments">
+            {this.state.comments.map(this.renderComment)}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComment(comment) {
+    return <li>{comment}</li>;
+  }
+
+  toggleShowComments() {
+    this.setState({showComments: !this.state.showComments});
   }
 
   render() {
     return (
       <div className="article-links">
-        <a className="article-link" href="#">
+        <a className="article-link" onClick={this.toggleShowComments.bind(this)}>
           <i className="fa fa-comments-o"></i>
           <span className="article-link-text">Comments</span>
         </a>
@@ -18,13 +54,7 @@ class ArticleLinks extends Component {
           <i className="fa fa-share"></i>
           <span className="article-link-text">Share Post</span>
         </a>
-        <form onSubmit={this.addComment.bind(this)}>
-          <textarea className="comment" placeholder="Type your comment here" ref={(input) => this.commentText = input}></textarea>
-          <button type="submit" className="article-link">
-            <i className="fa fa-check"></i>
-            <span className="article-link-text">Submit</span>
-          </button>
-        </form>
+        {this.renderComments()}
       </div>
     );
   }
