79e6b2033d43464a695d4d350d70a42f0f05f9cb
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-23 14:11:34 -0400

    Move articles from state to props of OtherArticles

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,40 @@ import Article from './Article';
 import Aside from './Aside';
 import OtherArticles from './OtherArticles';
 
+const articles = [
+  {
+    title: "Single Orcs in Indianapolis",
+    href: "https://mitzialexander.itch.io/orc-dating-sim",
+    img: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+    alt: "orc",
+  },
+  {
+    title: "You won't believe what's under this mountain",
+    href: "https://en.wikipedia.org/wiki/Lonely_Mountain",
+    img: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+    alt: "mountain",
+  },
+  {
+    title: "Mine 20% more gold with One Weird Trick",
+    href: "http://www.mygoldpanning.com/tipstricksandsecretstofindinggold.html",
+    img: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+    alt: "gold",
+  },
+  {
+    title: "Surprise for Indiana Hobbits born before 1999",
+    href: "http://www.dailymail.co.uk/news/article-2261862/So-Mrs-Bilbo-DO-live-like-Hobbit--The-Oxford-graduate-whos-lived-13-years-mud-hut--really-away-fairies.html",
+    img: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+    alt: "hobbit",
+  }
+];
+
 class Main extends Component {
   render() {
     return (
     <main className="expanded row">
       <Article />
       <Aside />
-      <OtherArticles />
+      <OtherArticles articles={articles} />
     </main>
     );
   }
--- src/OtherArticles.js
+++ src/OtherArticles.js
@@ -2,38 +2,6 @@ import React, { Component } from 'react';
 import './OtherArticles.css';
 
 class OtherArticles extends Component {
-  constructor() {
-    super()
-    this.state = {
-      articles: [
-        {
-          title: "Single Orcs in Indianapolis",
-          href: "#",
-          img: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
-          alt: "orc",
-        },
-        {
-          title: "You won't believe what's under this mountain",
-          href: "#",
-          img: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
-          alt: "mountain",
-        },
-        {
-          title: "Mine 20% more gold with One Weird Trick",
-          href: "#",
-          img: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
-          alt: "gold",
-        },
-        {
-          title: "Surprise for Indiana Hobbits born before 1999",
-          href: "#",
-          img: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
-          alt: "hobbit",
-        }
-      ],
-    }
-  }
-
   renderArticle(article, i) {
     return (
       <div className="small-6 medium-3 columns other-article" key={i}>
@@ -49,7 +17,7 @@ class OtherArticles extends Component {
     return (
     <div className="small-12 columns other-articles">
       <h2>From around the Realm</h2>
-      {this.state.articles.map((article, i) => this.renderArticle(article, i))}
+      {this.props.articles.map((article, i) => this.renderArticle(article, i))}
     </div>
     );
   }
