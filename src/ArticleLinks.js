2c7da4d308d77102671fbdcb50b74e943af02022
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-23 10:28:01 -0400

    Add text area and submit button for comments

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './ArticleLinks.css';
 
 class ArticleLinks extends Component {
+  addComment(e) {
+    e.preventDefault();
+    console.log(this.commentText.value);
+  }
+
   render() {
     return (
       <div className="article-links">
@@ -13,6 +18,13 @@ class ArticleLinks extends Component {
           <i className="fa fa-share"></i>
           <span className="article-link-text">Share Post</span>
         </a>
+        <form onSubmit={this.addComment.bind(this)}>
+          <textarea className="comment" placeholder="Type your comment here" ref={(input) => this.commentText = input}></textarea>
+          <button type="submit" className="article-link">
+            <i className="fa fa-check"></i>
+            <span className="article-link-text">Submit</span>
+          </button>
+        </form>
       </div>
     );
   }
