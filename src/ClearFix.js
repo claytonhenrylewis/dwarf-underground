625b5b04fb428fe1b2159643bf699d65ec626a29
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-23 13:44:03 -0400

    Render header links using map

--- src/ClearFix.js
+++ src/ClearFix.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import NavLinks from './NavLinks';
 import './ClearFix.css';
 
+const links = ['home', 'about', 'log in'];
+
 class ClearFix extends Component {
   render() {
     return (
@@ -9,7 +11,7 @@ class ClearFix extends Component {
         <div className="float-left logo">
           <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
         </div>
-        <NavLinks />
+        <NavLinks links={links}/>
       </div>
     );
   }
